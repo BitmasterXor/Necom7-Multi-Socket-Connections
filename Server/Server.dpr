4dd6fdb39474728cabe078acb3b4f10844335c05
BitmasterXor <81372201+BitmasterXor@users.noreply.github.com>
2024-05-23 19:02:07 -0500

    Add files via upload

---
+++ Server/Server.dpr
@@ -0,0 +1,17 @@
+program Server;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows11 Impressive Dark');
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
